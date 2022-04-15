9f38e0ab8b219ecf502901ae933966403d4d1a43
Antonin Stefanutti <antonin@stefanutti.fr>
2022-04-15 15:40:05 +0200

    Add process and runtime metrics

---
+++ cmd/kcp-glbc/metrics.go
@@ -0,0 +1,31 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus/collectors"
+
+	"github.com/kuadrant/kcp-glbc/pkg/metrics"
+)
+
+func init() {
+	// Register metrics with the global prometheus registry
+	metrics.Registry.MustRegister(
+		// expose process metrics like CPU, Memory, file descriptor usage etc.
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+		// expose Go runtime metrics like GC stats, memory stats etc.
+		collectors.NewGoCollector(),
+	)
+}
